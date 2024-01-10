4314c9467820a3ffb1b9a8aa349dc0814bec2c3c
zhafirdaffa <153572617+zhafirdaffa@users.noreply.github.com>
2024-01-09 17:32:14 -0800

    Add files via upload

---
+++ ganjil genap.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ 	
+	int bil_ganjil, bil_genap;
+	
+	cout << "Masukan Bilangan ganjil : ";
+	cin >> bil_ganjil;
+	cout << endl;
+	
+	cout << "Masukan bilangan genap : ";
+	cin >> bil_genap;
+	cout << endl;
+	
+	if (bil_ganjil %2==1 && bil_genap %2==0){
+		
+		cout << "kedua angka adalah bilangan ganjil dan genap ";
+		
+	}else {
+		
+		cout << " kedua angka bukan bilangan ganjil dan genap ";
+        	
+	} else if {
+		
+		cout << " Adalah bilangan ganjil";
+		
+		} else if {
+			
+			cout << " Adalah bilangan genap";
+		} 
+			
+		
+	
+	return 0;
+}
